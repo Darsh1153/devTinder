caa619dd1c6721a0f406a08348fc9ad0f9871570
Darshan <darsh19102002@gmail.com>
2025-08-18 16:24:59 +0530

    minor changes on signup

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const authRouter = express.Router();
 const User = require("../models/user");
-const {validateSignUpData} = require("../utils/validations");
+const { validateSignUpData } = require("../utils/validations");
 const bcrypt = require("bcrypt");
 
-authRouter.post("/signup", async (req, res) => {  
+authRouter.post("/signup", async (req, res) => {
 
     try {
         const { firstName, lastName, emailId, password } = req.body;
@@ -21,8 +21,10 @@ authRouter.post("/signup", async (req, res) => {
             emailId,
             password: hashedPassword
         })
-        await user.save();
-        res.send("User created successfully");
+        const savedUser = await user.save();
+        const jwtToken = await user.getJWT();
+        res.cookie("token", jwtToken);
+        res.send(savedUser);
     } catch (err) {
         res.status(400).send("Error creating user: " + err);
     }
